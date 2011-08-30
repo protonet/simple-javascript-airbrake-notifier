9d2e45daaa5295f48c1ccc8bfd8a0639a729d20f
Christopher Blum <christopher.blum@xing.com>
2011-08-30 14:46:55 +0200

    Readme and further minification

--- notifier.js
+++ notifier.js
@@ -1,72 +1,52 @@
-(function(window, location) {
-  var root          = location.protocol + "//" + location.host,
-      apiKey        = window.AIRBRAKE_API_KEY,
-      oldOnError    = window.onerror || function() {},
-      URL           = location.protocol + "//hoptoadapp.com/notifier_api/v2/notices?data=",
-      XML_TEMPLATE  = '<?xml version="1.0" encoding="UTF-8"?>'
-                      + '<notice version="2.0">'
-                      +   '<api-key>#{API_KEY}</api-key>'
-                      +   '<notifier>'
-                      +     '<name>hoptoad_notifier_js</name>'
-                      +     '<version>0.1.0</version>'
-                      +     '<url>http://hoptoadapp.com</url>'
-                      +   '</notifier>'
-                      +   '<error>'
-                      +     '<class>Error</class>'
-                      +     '<message>#{EXCEPTION_MESSAGE}</message>'
-                      +     '<backtrace>#{BACKTRACE_LINES}</backtrace>'
-                      +   '</error>'
-                      +   '<request>'
-                      +     '<component>frontend</component>'
-                      +     '<action>javascript</action>'
-                      +     '<url>#{REQUEST_URL}</url>'
-                      +     '<cgi-data>'
-                      +       '<var key="HTTP_USER_AGENT">#{USER_AGENT}</var>'
-                      +       '<var key="HTTP_REFERER">#{REFERER}</var>'
-                      +     '</cgi-data>'
-                      +   '</request>'
-                      +   '<server-environment>'
-                      +     '<project-root>#{PROJECT_ROOT}</project-root>'
-                      +     '<environment-name>production</environment-name>'
-                      +   '</server-environment>'
-                      + '</notice>';
+(function(window, location, undef) {
+  var root   = location.protocol + "//" + location.host,
+      apiKey = window.AIRBRAKE_API_KEY;
+  
+  function xmlNode(nodeName, attributes, nodeValue) {
+    attributes = attributes ? " " + attributes : "";
+    return "<" + nodeName + attributes +  ">" + nodeValue + "</" + nodeName + ">";
+  }
   
   function escapeText(text) {
-    return (text + "").replace(/[&,<,>,',"]/g, function(match) {
-      return "&#" + match.charCodeAt(0) + ";";
+    return (text + "").replace(/[&<>'"]/g, function(match) {
+      return "&#" + match.charCodeAt() + ";";
     });
   }
   
   function getXML(message, file, line) {
     file = file.replace(root, "[PROJECT ROOT]");
-    var mapping = {
-      API_KEY:            apiKey,
-      EXCEPTION_MESSAGE:  message,
-      REQUEST_URL:        location.href,
-      USER_AGENT:         navigator.userAgent,
-      REFERER:            document.referrer,
-      PROJECT_ROOT:       root,
-      BACKTRACE_LINES:    '<line method="" file="' + escapeText(file) + '" number="' + escapeText(line) + '" />'
-    };
-    
-    return XML_TEMPLATE.replace(/#{(.+?)}/g, function(match, $1) {
-      return mapping[$1];
-    });
+    return '<?xml version="1.0" encoding="UTF-8"?>' +
+            xmlNode("notice", 'version="2.0"',
+              xmlNode("api-key",  undef, apiKey) +
+              xmlNode("notifier", undef,
+                xmlNode("name",     undef, "hoptoad_notifier_js")   +
+                xmlNode("version",  undef, "0.1.0")                 +
+                xmlNode("url",      undef, "http://hoptoadapp.com")
+              ) +
+              xmlNode("error",    undef, 
+                xmlNode("class",      undef, "Error")    +
+                xmlNode("message",    undef, escapeText(message))    +
+                xmlNode("backtrace",  undef, '<line method="" file="' + escapeText(file) + '" number="' + escapeText(line) + '" />')
+              ) +
+              xmlNode("request",  undef,
+                xmlNode("component",  undef, "frontend")    +
+                xmlNode("action",     undef, "javascript")  +
+                xmlNode("url",        undef, location.href) +
+                xmlNode("cgi-data",   undef, 
+                  xmlNode("var", 'key="HTTP_USER_AGENT"', navigator.userAgent) +
+                  xmlNode("var", 'key="HTTP_REFERER"',    document.referrer)
+                )
+              ) +
+              xmlNode("server-environment", undef,
+                xmlNode("project-root",     undef, root) +
+                xmlNode("environment-name", undef, "production")
+              )
+            );
   }
   
-  function notify(message, file, line) {
+  window.onerror = (window.Airbrake = {}).notify = function(message, file, line) {
     if (apiKey) {
-      new Image().src = URL + escape(getXML(message, file, line));
+      new Image().src = "//hoptoadapp.com/notifier_api/v2/notices?data=" + escape(getXML(message, file, line));
     }
-  }
-  
-  window.onerror = function() {
-    var args = arguments;
-    notify.apply(this, args);
-    oldOnError.apply(this, args);
-  };
-  
-  window.Airbrake = {
-    notify: notify
   };
 })(this, location);
